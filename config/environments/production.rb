f1846803eb9b15eee1e9925950d65f5fa678636c
sharshi <sharshib@gmail.com>
2020-02-09 17:09:29 -0500

    heroku prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,15 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
