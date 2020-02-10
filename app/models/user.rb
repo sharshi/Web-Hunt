8d32290b390715e3378c4209e11f7793f11ecff4
sharshi <sharshib@gmail.com>
2020-02-10 14:08:08 -0500

    user model figvaper

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,35 @@
 #
 
 class User < ApplicationRecord
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true }
+  attr_reader :password
+  before_validation :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    @user && @user.is_password?(password) ? @user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  def is_password?(password)
+    digest = BCrypt::Password.new(self.password_digest)
+    digest.is_password?(password)
+  end
 end
