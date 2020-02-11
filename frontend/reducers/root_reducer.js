23fa410911cadc206c258a94074413fc33c0c3d3
sharshi <sharshib@gmail.com>
2020-02-10 23:45:54 -0500

    todos for modal and reducer shape

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,6 +2,22 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import sessionsReducer from "./sessions_reducer";
 
+// TODO
+/**  
+ * Set state shape as determined here:
+ * https://github.com/sharshi/Web-Hunt/wiki/sample-state
+ * root
+ * - entities
+ * -- products
+ * -- users
+ * -- reviews
+ * -- topics
+ * -- productsTopics
+ * - ui
+ * - errors
+ * - session
+ */
+
 const rootReducer = combineReducers({
   users: usersReducer,
   sessions: sessionsReducer
