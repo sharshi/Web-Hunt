ec6b6ed9887321ea76c4d8e04c1f834bccc7b381
sharshi <sharshib@gmail.com>
2020-02-11 17:13:22 -0500

    footer style

--- frontend/components/structure/footer/footer.jsx
+++ frontend/components/structure/footer/footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-
 class Footer extends React.Component {
   render() {
+    const sites = {
+      github: 'https://github.com/sharshi',
+      linkedin: 'https://www.linkedin.com/in/sharshi/',
+      portfolio: 'http://www.sharshi.com/'
+    };
+    const links = Object.keys(sites).map(site => {
+      return (
+        <span key={site}> | <a target='_blank' href={sites[site]}>{site}</a></span>
+      )
+    })
+
     return (
       <footer className="footer">
-        footer
+        <section>
+          &copy; 2020 {links}
+        </section>
       </footer>
     )
   }
