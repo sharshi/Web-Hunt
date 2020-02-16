461b3cbfad9979eac30dcc914c44abb2b301a27c
sharshi <sharshib@gmail.com>
2020-02-16 02:34:18 -0500

    creates topics table

---
+++ app/models/topic.rb
@@ -0,0 +1,3 @@
+class Topic < ApplicationRecord
+  validates :name, presence: true 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_022433) do
+ActiveRecord::Schema.define(version: 2020_02_16_073213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2020_02_12_022433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hunter_id"], name: "index_products_on_hunter_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
