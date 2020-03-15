4379b6680ccf0a96dee3f4308ca52f4794ee06cf
sharshi <sharshib@gmail.com>
2020-03-15 17:44:30 -0400

    vote actions cleanup

--- frontend/actions/vote_actions.js
+++ frontend/actions/vote_actions.js
@@ -1,6 +1,12 @@
 import * as VoteUtil from "../utils/vote_util";
 
 export const RECEIVE_VOTE = "UPVOTE";
+export const RECEIVE_VOTE_ERRORS = "RECEIVE_VOTE_ERRORS";
+
+const voteErrors = errors => ({
+  type: RECEIVE_VOTE_ERRORS,
+  errors
+});
 
 export const receiveVote = (vote) => {
   return {
@@ -11,6 +17,6 @@ export const receiveVote = (vote) => {
 
 export const vote = vote => dispatch =>
          VoteUtil.vote(vote).then(
-           (vote) => dispatch(receiveVote(vote)),
-           errors => dispatch(productErrors(errors.responseJSON))
+           vote => dispatch(receiveVote(vote)),
+           errors => dispatch(voteErrors(errors.responseJSON))
          );
