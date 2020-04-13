54a7ee0b129242f32fe78c4dd89b644d2a825612
sharshi <sharshib@gmail.com>
2020-04-13 16:12:32 -0400

    fix navbar profilepicture update

--- frontend/components/structure/nav/nav_bar_container.js
+++ frontend/components/structure/nav/nav_bar_container.js
@@ -4,11 +4,12 @@ import { logoutUser, openModal } from "../../../actions/session_actions";
 import { openUserMenu, closeUserMenu } from "../../../actions/navbar_actions";
 
 const mapStateToProps = state => {
-  // const pic = state.session.currentUser.id ? state.entities.users[state.session.currentUser.id].profilePictureUrl : window.pp;
+  const pic = state.session.currentUser.id ? state.entities.users[state.session.currentUser.id].profilePictureUrl : null;
+
   return {
     currentUserId: state.session.currentUser.id,
     currentUserName: state.session.currentUser.username,
-    profilePictureUrl: state.session.currentUser.profilePictureUrl,
+    profilePictureUrl: pic,
     userMenuStatus: state.ui.navBar === "opened"
   };}
 
